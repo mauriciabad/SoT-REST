b2bb6cdfdc03ef9b0a9d66b79cdb8284fda849ec
Maurici <hello@mauriciabad.com>
2019-10-01 19:44:26 +0200

    Added bunch of menus

--- client/src/main/java/Interface.java
+++ client/src/main/java/Interface.java
@@ -12,36 +12,110 @@ public class Interface {
 
         console.write("Welcome to Airline API ✈\n");
 
-        goToMainMenu();
+        goToMenuMain();
 
         console.write("\nThank you for using our API 😄");
     }
 
-    private void goToMainMenu() {
-        console.write("What do you want to do?");
-        console.write("  1. Create a flight");
-        console.write("  2. Delete a flight");
-        console.write("  3. Read a flight");
+    private void goToMenuMain() {
+        console.write("What Entity you want to work with?");
+        console.write("  1. Flights");
+        console.write("  2. Users");
         console.write("  exit. Close");
 
-        String action = console.read();
-
-        switch (action){
+        switch (console.read().toLowerCase()){
             case "exit": return;
-            case "1":
-                console.write("You selected CREATE");
-                break;
-            case "2":
-                console.write("You selected DELETE");
-                break;
-            case "3":
-                console.write("You selected READ");
-                break;
-            default:
-                console.write("Invalid option, try again\n");
-                goToMainMenu();
-                break;
+            case "1": goToMenuFlight(); break;
+            case "2": goToMenuUser(); break;
+            default: console.write("Invalid option, try again\n"); goToMenuMain(); break;
         }
+    }
 
+    private void goToMenuFlight() {
+        console.write("What do you want to do?");
+        console.write("  C. Create a flight");
+        console.write("  R. Read a flight");
+        console.write("  U. Update a flight");
+        console.write("  D. Delete a flight");
+        console.write("  back. Go to flight's menu");
+        console.write("  exit. Close");
+
+        switch (console.read().toLowerCase()){
+            case "exit": return;
+            case "back": goToMenuMain(); break;
+            case "c": goToMenuFlightCreate(); break;
+            case "r": goToMenuFlightRead(); break;
+            case "u": goToMenuFlightUpdate(); break;
+            case "d": goToMenuFlightDelete(); break;
+            default: console.write("Invalid option, try again\n"); goToMenuFlight(); break;
+        }
+    }
+
+    private void goToMenuFlightCreate() {
+        console.write("Create Flight menu");
+        console.write("Work in progress 🛠‍, try again in next update. 👍");
+        goToMenuFlight();
+    }
+
+    private void goToMenuFlightRead() {
+        console.write("Read Flight menu");
+        console.write("Work in progress 🛠, try again in next update. 👍");
+        goToMenuFlight();
+    }
+
+    private void goToMenuFlightUpdate() {
+        console.write("Update Flight menu");
+        console.write("Work in progress 🛠, try again in next update. 👍");
+        goToMenuFlight();
+    }
+
+    private void goToMenuFlightDelete() {
+        console.write("Delete Flight menu");
+        console.write("Work in progress 🛠, try again in next update. 👍");
+        goToMenuFlight();
+    }
+
+    private void goToMenuUser() {
+        console.write("What do you want to do?");
+        console.write("  C. Create a user");
+        console.write("  R. Read a user");
+        console.write("  U. Update a user");
+        console.write("  D. Delete a user");
+        console.write("  back. Go to user's menu");
+        console.write("  exit. Close");
+
+        switch (console.read().toLowerCase()){
+            case "exit": return;
+            case "back": goToMenuMain(); break;
+            case "c": goToMenuUserCreate(); break;
+            case "r": goToMenuUserRead(); break;
+            case "u": goToMenuUserUpdate(); break;
+            case "d": goToMenuUserDelete(); break;
+            default: console.write("Invalid option, try again\n"); goToMenuFlight(); break;
+        }
+    }
+
+    private void goToMenuUserCreate() {
+        console.write("Create User menu");
+        console.write("Work in progress 🛠‍, try again in next update. 👍");
+        goToMenuUser();
+    }
+
+    private void goToMenuUserRead() {
+        console.write("Read User menu");
+        console.write("Work in progress 🛠, try again in next update. 👍");
+        goToMenuUser();
+    }
+
+    private void goToMenuUserUpdate() {
+        console.write("Update User menu");
+        console.write("Work in progress 🛠, try again in next update. 👍");
+        goToMenuUser();
+    }
+
+    private void goToMenuUserDelete() {
+        console.write("Delete User menu");
+        console.write("Work in progress 🛠, try again in next update. 👍");
+        goToMenuUser();
     }
 }
