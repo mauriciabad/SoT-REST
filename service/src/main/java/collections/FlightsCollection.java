80cda0562c7bf5a8c9c820f6400263d77cbdff42
Maurici <hello@mauriciabad.com>
2019-10-04 02:24:34 +0200

    Update initial flights

--- service/src/main/java/collections/FlightsCollection.java
+++ service/src/main/java/collections/FlightsCollection.java
@@ -18,13 +18,13 @@ public class FlightsCollection {
 
     // Initialize with some data
     private FlightsCollection() {
-        this.add(new Flight("BCN", "EIN", "2019-09-28T08:20", "2019-09-28T10:20", "Ryanair", 2, 120));
-        this.add(new Flight("LHR", "CDG", "2019-09-29T10:10", "2019-09-29T12:10", "Transavia", 12, 60));
-        this.add(new Flight("FRA", "MAD", "2019-09-28T08:25", "2019-09-28T13:10", "Ryanair", 6, 40));
-        this.add(new Flight("ATL", "BCN", "2019-10-01T21:50", "2019-10-01T22:50", "Vueling", 14, 120));
-        this.add(new Flight("HND", "PVG", "2019-10-13T17:30", "2019-10-13T108:0", "Vueling", 4, 95));
-        this.add(new Flight("BCN", "MAD", "2019-10-25T12:15", "2019-10-25T13:30", "Transavia", 10, 15));
-        this.add(new Flight("EIN", "BCN", "2020-10-19T06:45", "2019-10-19T15:30", "Ryanair", 7, 30));
+        this.add(new Flight("BCN", "EIN", "2019-09-28 08:20", "2019-09-28 10:20", "Ryanair", 2, 120));
+        this.add(new Flight("LHR", "CDG", "2019-09-29 10:10", "2019-09-29 12:10", "Transavia", 1, 60));
+        this.add(new Flight("FRA", "MAD", "2019-09-28 08:25", "2019-09-28 13:10", "Ryanair", 3, 40));
+        this.add(new Flight("ATL", "BCN", "2019-10-01 21:50", "2019-10-01 22:50", "Vueling", 1, 120));
+        this.add(new Flight("HND", "PVG", "2019-10-13 17:30", "2019-10-13 108:0", "Vueling", 2, 95));
+        this.add(new Flight("BCN", "MAD", "2019-10-25 12:15", "2019-10-25 13:30", "Transavia", 2, 15));
+        this.add(new Flight("EIN", "BCN", "2020-10-19 06:45", "2019-10-19 15:30", "Ryanair", 25, 30));
     }
 
     // Data structure containing flights
